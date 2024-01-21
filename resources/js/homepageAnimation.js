f969e3298dd75c0ba3cdd6cfc5bee1c3f1b2a35c
Zander Coffman <zandergalvez330@gmail.com>
2024-01-21 09:23:50 -0700

    frontpage animation finished

---
+++ resources/js/homepageAnimation.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const oSpan = document.getElementById("o-characters");
+  
+    const getDelay = (ms) => {
+      return new Promise(resolve => setTimeout(resolve, ms));
+    }
+  
+    async function commitOAnimation() {
+        if (oSpan.textContent.length * 80 >= (window.innerWidth - 11) - 80) {
+            document.documentElement.style.setProperty('--intro-visible', 'visible');
+            document.querySelector('.introduction').classList.add('fadeIn');
+            return;
+        }
+  
+        await getDelay(150);
+        oSpan.textContent = oSpan.textContent + "O";
+        commitOAnimation();
+    }
+  
+    commitOAnimation();
+});
+  
